1d0c688133688710531e8beede0b31e9d4fa2af3
Chris Moyer <cmoyer@newstex.com>
2016-07-06 10:03:08 -0400

    Added some more complex tracing options

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # express-appenlight
 Support for the AppEnlight Metrics API for applications using express.js
+
+
+## Initializing AppEnlight Tracer:
+
+
+All that's needed to enable the tracer is to initialize it and set it up as middleware for your Express.js app:
+
+```
+var AppEnlight = require('express-appenlight');
+app.use(new AppEnlight(conf.APPENLIGHT_KEY));
+```
+
+Once that's set up, every request will have an *ae_tracer* option.
+
+## Tracing a custom function
+
+Some functions are automatically traced, however to add in a custom trace you can use the *ae_tracer* function available on every request object.
+
+Usage:
+
+```
+function (req, res, next){
+	// Add a new trace
+	var trace_completed = req.ae_tracer.trace('functionName');
+
+	// Do your application logic
+	... do stuff...
+
+	// Call when everything is completed
+	trace_completed();
+}
+```
--- index.js
+++ index.js
@@ -11,28 +11,43 @@ const hostname = require('os').hostname();
 
 const API_ENDPOINT = 'https://api.appenlight.com/api/request_stats?protocol_version=0.5';
 
-function AppEnlight(api_key){
-	this.api_key = api_key;
-	var self = this;
-
-	/**
-	 * Router middleware for Express.js
-	 */
-	return function router(req, res, next){
-		var start_time = new Date();
-		res.on('finish', function(){
-			var completion_time = new Date() - start_time;
-			self.sendMetrics(req, res, completion_time);
-		});
-		next();
-	};
+/**
+ * AppEnlight Tracer, exposed as req.ae_tracer,
+ * allows tracing individual function calls within an express.js app
+ */
+function AppEnlightTracer(name){
+	this.start_time = new Date();
+	this.metrics = [[
+		name,
+		{}
+	]];
 }
 
 /**
- * Send metrics to AppEnlight
+ * Trace an indivudal function
+ *
+ * @param name: Name of the function to trace
+ * @return: Callback function to execute on completion of function
  */
-AppEnlight.prototype.sendMetrics = function sendMetrics(req, res, completion_time){
+AppEnlightTracer.prototype.trace = function ae_trace(name){
+	var self = this;
+	var trace_start = new Date();
+	return function trace_done(){
+		var completion_time = new Date() - trace_start;
+		self.metrics.push([
+			name, {
+				custom: completion_time/100,
+			},
+		]);
+	};
+};
+
+/**
+ * Mark this request as completed and send metrics to AppEnlight
+ */
+AppEnlightTracer.prototype.done = function ae_done(){
 	var now = new Date();
+	this.metrics[0][1].custom = (now-this.start_time)/100;
 	request({
 		method: 'POST',
 		uri: API_ENDPOINT,
@@ -42,15 +57,26 @@ AppEnlight.prototype.sendMetrics = function sendMetrics(req, res, completion_tim
 		json: [{
 			server: hostname,
 			timestamp: now.toISOString(),
-			metrics: [
-				[[req.method, req.path].join(':'),
-					{
-						custom: completion_time/100,
-					},
-				],
-			],
+			metrics: this.metrics,
 		}],
 	});
 };
+
+function AppEnlight(api_key){
+	this.api_key = api_key;
+	var self = this;
+
+	/**
+	 * Router middleware for Express.js
+	 */
+	return function router(req, res, next){
+		var routeName = [req.method, req.path].join(':');
+		req.ae_tracer = new AppEnlightTracer(routeName);
+		res.on('finish', function(){
+			req.ae_tracer.done();
+		});
+		next();
+	};
+}
 
 module.exports = AppEnlight;
